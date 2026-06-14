bfb2e7bc9a01353b9de174771aeec8d66fc1e126
xck8284 <xck8284@gmail.com>
2026-06-14 15:41:45 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { bullishStocks, bearishStocks } from "./analysisData";
-
+const API_BASE = "https://zhu-stock-app.onrender.com";
 
 
 function App() {

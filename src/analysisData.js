3f6ecb6a48fd0aefca339b2787d66d6460752e62
xck8284 <xck8284@gmail.com>
2026-06-14 15:09:13 +0800

    Create analysisData.js

---
+++ src/analysisData.js
@@ -0,0 +1,40 @@
+export const bullishStocks = [
+  {
+    stock_id: "2330",
+    name: "台積電",
+    stars: "★★★★★",
+    strong_score: 128,
+    bias: "12%",
+  },
+  {
+    stock_id: "3017",
+    name: "奇鋐",
+    stars: "★★★★☆",
+    strong_score: 115,
+    bias: "18%",
+  },
+  {
+    stock_id: "3661",
+    name: "世芯",
+    stars: "★★★★★",
+    strong_score: 135,
+    bias: "9%",
+  },
+];
+
+export const bearishStocks = [
+  {
+    stock_id: "2603",
+    name: "長榮",
+    stars: "★★★★☆",
+    strong_score: 102,
+    bias: "-9%",
+  },
+  {
+    stock_id: "2615",
+    name: "萬海",
+    stars: "★★★★☆",
+    strong_score: 96,
+    bias: "-7%",
+  },
+];

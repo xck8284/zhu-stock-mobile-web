21b86c0748fee82c023b1f457b5ce421c4d8916a
xck8284 <xck8284@gmail.com>
2026-06-28 14:22:53 +0800

    Create mobileApi.js

---
+++ src/mobileApi.js
@@ -0,0 +1,16 @@
+const API_BASE = "https://zhu-stock-app.onrender.com";
+
+export async function adminFetch(url, token) {
+  const res = await fetch(`${API_BASE}${url}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "X-Mobile": "true",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
+
+  return await res.json();
+}
